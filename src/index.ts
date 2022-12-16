b84663f6e3a11277495d0f3600f75c2979eb5a8d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-12-16 18:06:56 +0100

    add 3rd-level call to getSongs()

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,21 @@ async function getSongs(): Promise<Song[]> {
   return loadedSongs;
 }
 
+// 1st-level caller (reported by existing eslint plugins)
 async function getOfficialVideos(): Promise<Song[]> {
   return (await getSongs()).filter((song) =>
     song.name.match(/official video/i)
   );
 }
 
+// 2nd-level caller (NOT reported by existing eslint plugins)
+async function searchOfficialVideos(searchTerm: string): Promise<Song[]> {
+  return (await getOfficialVideos()).filter((song) =>
+    song.name.match(searchTerm)
+  );
+}
+
+// 3rd-level caller (NOT reported by existing eslint plugins)
 (async () => {
-  console.log(await getOfficialVideos()); // => we're not warned about getOfficialVideos() relying on a deprecated function
+  console.log(await searchOfficialVideos("Alvvays"));
 })();
