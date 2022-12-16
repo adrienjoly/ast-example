c84aef68edea179d4299c9228b081024e36c1a30
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-12-16 17:39:31 +0100

    wip: recursion strategy (pseudo-code)

--- src/02-detect-all-callers.ts
+++ src/02-detect-all-callers.ts
@@ -8,17 +8,23 @@ const tsConfigFilePath = "./tsconfig.json";
 const targetFile = "./src/index.ts";
 const targetFunction = "getSongs";
 
-const directCallers =
+const directRefs =
   new tsmorph.Project({ tsConfigFilePath })
     .getSourceFile(targetFile)
     ?.getFunction(targetFunction)
     ?.findReferencesAsNodes() || [];
 
-const allCallers = [...directCallers]; // TODO: also include indirect callers
+const allRefs = [...directRefs]; // TODO: also include indirect callers
+
+for (const directRef of allRefs) {
+  // 1. find the caller (i.e. function that called getSongs), for that direct reference
+  // 2. push all references to that caller, into allRefs
+  // 3. then, apply the same process to those references, until they are not referenced anywhere.
+}
 
 const renderCaller = (fctCall: tsmorph.Node) => ({
   file: fctCall.getSourceFile().getFilePath(),
   line: fctCall.getStartLineNumber(),
 });
 
-console.log(`callers of ${targetFunction}:`, allCallers.map(renderCaller));
+console.log(`callers of ${targetFunction}:`, allRefs.map(renderCaller));
